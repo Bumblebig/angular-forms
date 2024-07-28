0ac9b7663a3b9fb087ad295b68bb7513fa16ba41
Abbdurahman <dreydare16@gmail.com>
2024-07-28 13:59:58 +0100

    successfully called api to dropdown

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,20 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 // import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, HttpClientModule],
   templateUrl: './forms.component.html',
-  styleUrl: './forms.component.css'
+  styleUrls: ['./forms.component.css']
 })
-
-export class FormsComponent {
+export class FormsComponent implements OnInit {
   firstName: string = '';
   lastName: string = '';
   email: string = '';
@@ -22,8 +22,29 @@ export class FormsComponent {
   country: string = '';
   occupation: string = '';
   successful: string = '';
+  countries: any[] = [];
+  occupations: any[] = [];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
+  public data: any;
+
+  ngOnInit() {
+    this.fetchDetails()    
+  }
+
+  fetchDetails() {
+    this.http.get('https://restcountries.com/v3.1/all').subscribe(
+      (resp: any) => {
+        this.countries = resp.map((country: any) => ({
+          name: country.name.common,
+          code: country.cca2
+        }));
+      },
+      (error) => {
+        console.error('Error fetching country data:', error);
+      }
+    );
+  }
 
   onSubmit(form: NgForm) {
     if (form.valid) {
@@ -38,6 +59,5 @@ export class FormsComponent {
     } else {
       // this.toastr.warning('Please fill out all required fields.', 'Warning');
     }
-    }
+  }
 }
-
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import routeConfig from './app/app.routes';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routeConfig),
+    HttpClientModule,
   ]
 }).catch(err => console.error(err));
